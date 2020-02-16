ca1bb5f2ba251bcf6e8867d0e18dca3d29f86a7c
Jeff <jeff.sagal@briteweb.com>
2020-02-16 10:59:24 -0800

    chore: add babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: "vue-mutable.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
