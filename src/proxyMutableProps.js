9bb63da081466b7f73708c7532a016a78adf3828
Jeff <jeff.sagal@briteweb.com>
2020-02-16 14:16:39 -0800

    feat: set internal data properties

--- src/proxyMutableProps.js
+++ src/proxyMutableProps.js
@@ -2,14 +2,37 @@
  * @type {VueConstructor | ComponentOptions<Vue>}
  */
 const proxyMutableProps = {
-  beforeCreate() {
-    const shouldProxy = getProxyable(this.props);
+  data() {
+    const data = {};
+
+    getProxyable(this.__proto__.constructor.sealedOptions.props).forEach(
+      prop => (data[`_${prop}`] = undefined)
+    );
+
+    return data;
+  },
+  mounted() {
+    getProxyable(this.__proto__.constructor.sealedOptions.props).forEach(
+      prop => {
+        this.$data[`_${prop}`] = this[prop];
+      }
+    );
   }
 };
 
-const getProxyable = (props = {}) =>
-  Object.keys(props).filter(
+const getProxyable = props => {
+  return Object.keys(props).filter(
     prop => props[prop].hasOwnProperty("mutable") && props[prop].mutable
   );
+};
+
+/**
+ *
+ * @param propName {string}
+ * @param vue {Component}
+ */
+const setDataProperty = (propName, vue) => {
+  vue.$set(vue, `_${propName}`, vue[propName]);
+};
 
 export { proxyMutableProps, getProxyable };
--- test/proxyMutableProps.spec.js
+++ test/proxyMutableProps.spec.js
@@ -1,4 +1,20 @@
-import { getProxyable } from "../src/proxyMutableProps";
+import Vue from "vue";
+import { shallowMount } from "@vue/test-utils";
+import { getProxyable, proxyMutableProps } from "../src/proxyMutableProps";
+
+/**
+ *
+ * @type {VueConstructor}
+ */
+const Stub = Vue.component("stub", {
+  render: createElement => createElement("div"),
+  mixins: [proxyMutableProps],
+  props: {
+    myProp: {
+      mutable: true
+    }
+  }
+});
 
 test("it determines which props to proxy", () => {
   const proxyable = getProxyable({
@@ -8,4 +24,14 @@ test("it determines which props to proxy", () => {
   });
 
   expect(proxyable).toEqual(["shouldProxy"]);
+});
+
+test("it defines internal data properties for proxyable props", () => {
+  const Wrapper = shallowMount(Stub, { propsData: { myProp: "hello world" } });
+  expect(Wrapper.vm.$data).toHaveProperty("_myProp");
+});
+
+test("it sets internal data properties for proxyable props", () => {
+  const Wrapper = shallowMount(Stub, { propsData: { myProp: "hello world" } });
+  expect(Wrapper.vm.$data._myProp).toEqual("hello world");
 });
